f146460ad13e3b92b17652ef018ef970a6835970
tokumaga <tokumaga@yahoo-corp.jp>
2020-12-28 17:45:28 +0900

    inital

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+// import { signIn, signOut, useSession } from "next-auth/client";
+
+export default function Home() {
+  return (
+    <>
+      <div>サインインしてください</div>
+      <button>Sign in</button>
+    </>
+  );
+}
