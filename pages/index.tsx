8eeee34b02a418aac3d9215ced472b9117f5c787
tokumaga <tokumaga@yahoo-corp.jp>
2021-01-05 15:24:50 +0900

    auth user login

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
-// import { signIn, signOut, useSession } from "next-auth/client";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Home() {
+  const [session, loading] = useSession();
   return (
     <>
-      <div>サインインしてください</div>
-      <button>Sign in</button>
+      {!session && (
+        <>
+          サインインしてください。 <br />
+          <button onClick={signIn}>Sign in</button>
+        </>
+      )}
+      {session && (
+        <>
+          サインイン完了。 email: {session.user.email} <br />
+          <button onClick={signOut}>Sign out</button>
+        </>
+      )}
     </>
   );
 }
