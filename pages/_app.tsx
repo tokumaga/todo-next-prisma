0ec0f46bf3693186e6f1eb99baf223e23ec43936
tokumaga <tokumaga@yahoo-corp.jp>
2021-01-20 17:33:28 +0900

    add react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import { Provider } from "next-auth/client";
+import { QueryClient, QueryClientProvider} from "react-query";
+
+const queryClinet = new QueryClient();
+
 export default function App({ Component, pageProps }) {
   return (
-    <Provider session={pageProps.session}>
-      <Component {...pageProps} />
-    </Provider>
+    <QueryClientProvider client={queryClinet}>
+      <Provider session={pageProps.session}>
+        <Component {...pageProps} />
+      </Provider>
+    </QueryClientProvider>
   );
 }
